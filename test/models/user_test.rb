1e0aeea9bff9ec7bbe8d5d29aaa6a4ff24eb220e
Hienhpss <thanhhien008@gmail.com>
2016-08-23 04:58:02 +0000

    Finish chapter 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,11 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.create(name:"aaa", email:"aaa@aaa.com")
+    @user = User.create(name:"aaa", email:"aaa@aaa.com", password:"abcdefgh", password_confirmation:"abcdefgh")
+  end
+  
+  test "user should be valid" do
+    assert @user.valid?
   end
   
  test "name should not be blank" do
@@ -25,6 +29,31 @@ class UserTest < ActiveSupport::TestCase
   
   test "email should not longer than 255" do
     @user.email = "a"*244 + "@examplessis"
+    assert_not @user.valid?
+  end
+  
+  test "email should have valid format" do
+    email_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    email_addresses.each do |email_address|
+      @user.email = email_address
+      assert_not @user.valid?, "#{email_addresses.inspect} should be invalid"
+    end
+  end
+  
+  test "email should be unique" do
+    dup_user = @user.dup
+    assert_not dup_user.valid?
+  end
+  
+  test "case sensitive" do
+    sen_user = @user.dup
+    sen_user.email = @user.email.upcase
+    assert_not sen_user.valid?
+  end
+  
+  test "password should long enough" do
+    @user.password = "a"*7
     assert_not @user.valid?
   end
   
